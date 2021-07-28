b20f2fa803ebf9bf454ea8b9d1b2cda4460f69d0
LeeDowon <solar0t@naver.com>
2021-07-28 16:38:49 +0900

    add docker secret

--- Pinterest/settings/deploy.py
+++ Pinterest/settings/deploy.py
@@ -2,6 +2,17 @@
 
 from .base import *
 
+
+# 도커 시크릿 함수
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+
+    return secret
+
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -19,7 +30,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production! 배포 시에는 DEBUG = False
 DEBUG = False
@@ -35,7 +46,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'test123',
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
